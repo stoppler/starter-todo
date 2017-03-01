0f042d5c832777f4c1c398804633a7894aa20670
Ryan Stoppler <rstoppler01@gmail.com>
2017-03-01 10:10:51 -0800

    Updated maintenance page, added our own pagination

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -1,39 +1,71 @@
 <?php
-
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 /**
  * Description of Mtce
  *
  * @author ry
  */
-class Mtce extends Application {
+class Mtce extends Application
 
-        public function index()
-        {
-                $this->data['pagetitle'] = 'TODO List Maintenance';
-                $tasks = $this->tasks->all(); // get all the tasks
+{
+	private $items_per_page = 10;
+	public function index()
 
-                        // substitute the status name
-                        foreach ($tasks as $task) {
-                            if (!empty($task->status)) {
-                $task->status = $this->statuses->get($task->status)->name;
-            }
-        }
-
-        // build the task presentation output
-        $result = '';   // start with an empty array        
-        foreach ($tasks as $task) {
-                    $result .= $this->parser->parse('oneitem', (array) $task, true);
-                }
-
-        // and then pass them on
-        $this->data['display_tasks'] = $result;
-        $this->data['pagebody'] = 'itemlist';
-        $this->render();    
-        }
+	{
+		$this->page(1);
+	}
+	// Show a single page of todo items
+	private function show_page($tasks)
+	{
+		$this->data['pagetitle'] = 'TODO List Maintenance';
+		// build the task presentation output
+		$result = ''; // start with an empty array
+		foreach($tasks as $task) {
+			if (!empty($task->status)) {
+				$task->status = $this->statuses->get($task->status)->name;
+			}
+			$result.= $this->parser->parse('oneitem', (array)$task, true);
+		}
+		$this->data['display_tasks'] = $result;
+		// and then pass them on
+		$this->data['pagebody'] = 'itemlist';
+		$this->render();
+	}
+	// Extract & handle a page of items, defaulting to the beginning
+	function page($num = 1)
+	{
+		$records = $this->tasks->all(); // get all the tasks
+		$tasks = array(); // start with an empty extract
+		// use a foreach loop, because the record indices may not be sequential
+		$index = 0; // where are we in the tasks list
+		$count = 0; // how many items have we added to the extract
+		$start = ($num - 1) * $this->items_per_page;
+		foreach($records as $task) {
+			if ($index++ >= $start) {
+				$tasks[] = $task;
+				$count++;
+			}
+			if ($count >= $this->items_per_page) {
+				break;
+			}
+		}
+		$this->data['pagination'] = $this->pagenav($num);
+		$this->show_page($tasks);
+	}
+	// Build the pagination navbar
+	private function pagenav($num)
+	{
+		$lastpage = ceil($this->tasks->size() / $this->items_per_page);
+		$parms = array(
+			'first' => 1,
+			'previous' => (max($num - 1, 1)) ,
+			'next' => min($num + 1, $lastpage) ,
+			'last' => $lastpage
+		);
+		return $this->parser->parse('itemnav', $parms, true);
+	}
 }
