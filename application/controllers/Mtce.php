b014b03af704a0b410149effc1ebf877d0836541
Ryan Stoppler <rstoppler01@gmail.com>
2017-03-12 17:12:02 -0700

    completed job 12

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -78,4 +78,79 @@ class Mtce extends Application
 		);
 		return $this->parser->parse('itemnav', $parms, true);
 	}
+        // Initiate adding a new task
+public function add()
+{
+    $task = $this->tasks->create();
+    $this->session->set_userdata('task', $task);
+    $this->showit();
+}
+// initiate editing of a task
+public function edit($id = null)
+{
+    if ($id == null) {
+            redirect('/mtce');
+        }
+        $task = $this->tasks->get($id);
+    $this->session->set_userdata('task', $task);
+    $this->showit();
+}
+// Render the current DTO
+private function showit()
+{
+    $task = $this->session->userdata('task');
+    $this->data['id'] = $task->id;
+    foreach ($this->priorities->all() as $record)
+    {
+        $priparms[$record->id] = $record->name;
+    }
+    $fields = array(
+        'ftask' => makeTextField('Task description', 'task', $task->task, 'Work', "What needs to be done?"),
+        'fpriority' => makeComboBox('Priority', 'priority', $task->priority, $priparms, "How important is this task?"),
+        'zsubmit' => makeSubmitButton('Update the TODO task', "Click on home or <back> if you don't want to change anything!", 'btn-success'),
+    );
+    $this->data = array_merge($this->data, $fields);
+
+    $this->data['pagebody'] = 'itemedit';
+    $this->render();
+}
+// handle form submission
+public function submit()
+{
+    $this->load->library('form_validation');
+    $this->form_validation->set_rules($this->tasks->rules());
+    // retrieve & update data transfer buffer
+    $task = (array) $this->session->userdata('task');
+    $task = array_merge($task, $this->input->post());
+    $task = (object) $task;  // convert back to object
+    $this->session->set_userdata('task', (object) $task);
+    if ($this->form_validation->run())
+    {
+        if (empty($task->id))
+        {
+            $this->tasks->add($task);
+            $this->alert('Task ' . $task->id . ' added', 'success');
+        } else
+            $this->tasks->update($task);
+            $this->alert('Task ' . $task->id . ' updated', 'success');
+    } else
+    {
+        $this->alert('<strong>Validation errors!<strong><br>' . validation_errors(), 'danger');
+    }
+    $this->showit();
+}
+// Forget about this edit
+function cancel() {
+    $this->session->unset_userdata('task');
+    redirect('/mtce');
+}
+// Delete this item altogether
+function delete()
+{
+    $dto = $this->session->userdata('task');
+    $task = $this->tasks->get($dto->id);
+    $this->tasks->delete($task->id);
+    $this->session->unset_userdata('task');
+    redirect('/mtce');
+}
 }
--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -11,17 +11,32 @@ class Tasks extends MY_Model {
         
             foreach ($this->all() as $task)
             {
-                if ($task->status != 2)
-                    $undone[] = $task;
+                if ($task->status != 2) {
+                $undone[] = $task;
             }
-         
-            foreach ($undone as $task)
-                $task->group = $this->groups->get($task->group)->name;
-        
-            usort($undone, "orderByCategory");
-
-            foreach ($undone as $task)
-                $converted[] = (array) $task;
-            return $converted;
         }
+         
+        foreach ($undone as $task) {
+            $task->group = $this->groups->get($task->group)->name;
+        }
+
+        usort($undone, "orderByCategory");
+
+        foreach ($undone as $task) {
+            $converted[] = (array) $task;
+        }
+        return $converted;
+        }
+        
+        // provide form validation rules
+public function rules()
+{
+    $config = array(
+        ['field' => 'task', 'label' => 'TODO task', 'rules' => 'alpha_numeric_spaces|max_length[64]'],
+        ['field' => 'priority', 'label' => 'Priority', 'rules' => 'integer|less_than[4]'],
+        ['field' => 'size', 'label' => 'Task size', 'rules' => 'integer|less_than[4]'],
+        ['field' => 'group', 'label' => 'Task group', 'rules' => 'integer|less_than[5]'],
+    );
+    return $config;
+}
 }
